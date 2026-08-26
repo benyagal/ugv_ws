7838c6adfcf757a02596f70d6919e6be79baa9b1
benyagal <benyagal@github.com>
2026-08-26 18:42:46 +0200

    Revert LD19 port_name back to /dev/ttyACM0 (device now enumerates there again)

--- src/ugv_else/ldlidar/launch/ld19.launch.py
+++ src/ugv_else/ldlidar/launch/ld19.launch.py
@@ -32,7 +32,7 @@ def generate_launch_description():
         {'product_name': 'LDLiDAR_LD19'},
         {'topic_name': 'scan'},
         {'frame_id': 'base_lidar_link'},
-        {'port_name': '/dev/ttyACM1'},
+        {'port_name': '/dev/ttyACM0'},
         {'port_baudrate': 230400},
         {'laser_scan_dir': True},
         {'enable_angle_crop_func': True},
